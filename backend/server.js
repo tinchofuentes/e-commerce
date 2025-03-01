34b96ff49b9b55285c8d595b38ae7471bb4e6b65
Martín Fuentes <tinchofuentes@gmail.com>
2025-02-28 21:38:35 -0300

    configuración de Express

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 // Definir el puerto
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware para parsear JSON
 app.use(express.json());
+
+// Habilitar CORS temporalmente sin restricciones
+app.use(cors());
 
 // Ruta de prueba
 app.get('/ping', (req, res) => {
