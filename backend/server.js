60554c757c00e21c59f5634a03f63b4b1b7750c7
Martín Fuentes <tinchofuentes@gmail.com>
2025-03-01 11:39:37 -0300

    Agregué morgan para registrar peticiones en la consola

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -9,11 +10,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware para parsear JSON
 app.use(express.json());
-
+ // Middleware para logs de peticiones
+app.use(morgan('dev'));
 // Habilitar CORS temporalmente sin restricciones
 app.use(cors());
 
-// Ruta de prueba
+// Endpoint de prueba
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
